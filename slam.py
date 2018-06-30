a840cd9c348130c55b5f90b6d84eecd0d981fef7
Kravchenko Fyodor <udalite.feda@hotmail.com>
2018-06-30 21:18:42 +0800

    init pangolin to view the poses

--- slam.py
+++ slam.py
@@ -6,6 +6,10 @@ import time
 from display import Display
 from featureExtractor import Frame, frame_matches, IRt
 from utils import denormalize
+import OpenGL.GL as gl
+import pangolin
+
+from multiprocessing import Process, Queue
 
 H = 1920//2
 W = 1080//2
@@ -20,12 +24,49 @@ class Map(object):
     def __init__(self):
         self.frames = []
         self.points = []
+        self.viewer_init()
+
+    def viewer_init(self):
+        pangolin.CreateWindowAndBind('Main', 640, 480)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
+        self.scam = pangolin.OpenGlRenderState(
+            pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+            pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+        self.handler = pangolin.Handler3D(self.scam)
+
+        # Create Interactive View in window
+        self.dcam = pangolin.CreateDisplay()
+        self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+        self.dcam.SetHandler(self.handler)
+
+    def viewer_refresh(self):
+        ppts = np.array([d[:3, 3] for d in self.state[0]])
+        spts = np.array(self.state[1])
+
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        self.dcam.Activate(self.scam)
+
+        gl.glPointSize(10)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(ppts)
+
+        gl.glPointSize(2)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(spts)
+
+        pangolin.FinishFrame()
+
 
     def display(self):
+        poses, pts = [], []
         for f in self.frames:
-            print(f.id)
-            print(f.pose)
-            print()
+           poses.append(f.pose)
+        for p in self.points:
+            pts.append(p.pt)
+        self.state = poses, pts
+        self.viewer_refresh()
 
 
 m = Map()
@@ -35,7 +76,7 @@ display = Display(W, H)
 class Point(object):
     def __init__(self, m, loc):
         self.frames = []
-        self.xyz = loc
+        self.pt = loc
         self.idx = []
 
         self.id = len(m.points)
